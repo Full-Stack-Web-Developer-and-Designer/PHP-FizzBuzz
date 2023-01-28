443fe2192100f65d18445bb50a4b0ec838350f80
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-01-28 01:27:21 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<style>
+    body{ text-align: center;}
+</style>
+<?php
+echo "<h1>FizzBuzz using while loop</h1>";
+$i = 1;
+
+while ($i <= 100) {
+    if ( 0 == $i % 15 ) {
+        echo 'FizzBuzz';
+    } else if ( 0 == $i % 3 ) {
+        echo 'Fizz';
+    } else if ( 0 == $i % 5 ) {
+        echo 'Buzz';
+    } else {
+        echo $i;
+    }
+
+    echo '<br>';
+    $i++;
+}
